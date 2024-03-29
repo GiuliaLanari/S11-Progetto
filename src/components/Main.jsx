12e98643a3cc2db722b0c7829639d3acbf0328a8
GiuliaLanari <giulia.lanari96@gmail.com>
2024-03-29 12:56:12 +0100

    Add fetch

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -48,7 +48,7 @@ const Main = function () {
           <div className="row">
             <div className="col-10">
               <div id="hiphop">
-                <h2>#HipHop</h2>
+                <h2>HipHop</h2>
                 <div
                   className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
                   id="hipHopSection"
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,17 @@
+const initialState = {
+  search: {
+    ricerca: {},
+  },
+  handleSection: {
+    selezionato: [],
+  },
+};
+
+const mainReducer = function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default mainReducer;
